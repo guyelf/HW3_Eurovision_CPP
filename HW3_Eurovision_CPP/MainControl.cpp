4a1d4fc2583bcc1468f47ce22565861e86f4436e
Guy-El Fridman <guy-elf@campus.technion.ac.il>
2019-06-11 19:55:12 +0300

    Added functionality to MainControl

---
+++ HW3_Eurovision_CPP/MainControl.cpp
@@ -0,0 +1,31 @@
+#include "eurovision.h"
+
+MainControl::MainControl(int maxSongLength, int maxParticipants,
+	int maxRegularVotes): maxSongLength(maxSongLength), 
+						  maxParticipants(maxParticipants),
+						  maxRegularVotes(maxRegularVotes)
+{
+	this->phase = Registration;
+}
+
+void MainControl::setPhase(Phase phase)
+{
+	if ((phase - this->phase) != 1) return;
+	this->phase = phase;
+}
+
+bool MainControl::legalParticipant(Participant p)
+{
+	bool illigal_result = p.timeLength() > this->maxSongLength || p.state() == "" ||
+		p.song() == "" || p.singer() ==  "";
+
+	return !illigal_result;
+}
+
+MainControl& MainControl::operator+=(const Participant& p) 
+{
+	
+}
+
+
+
--- HW3_Eurovision_CPP/eurovision.h
+++ HW3_Eurovision_CPP/eurovision.h
@@ -2,6 +2,7 @@
 #define EUROVISION_H_
 
 #include <iostream>
+#include <string>
 
 // it's allowed to define here any using statements, according to needs.
 // do NOT define here : using namespace std;
@@ -19,15 +20,20 @@ enum Phase { Registration, Contest, Voting };
 {
 	// relevant private members can be defined here, if necessary.
 
-	 const string state;
-	 bool isRegistered;
+	 const string state_name;
+	 string song_name;
+	 int song_duration;
+	 string singer_name;
+	 bool is_registered;
 
 public:
 
-	string song;
-	int timeLength;
-	string singer;
-	
+	string state()const;
+	string song() const;
+	int timeLength() const;
+	string singer() const;
+	bool isRegistered() const;
+
 	Participant(const string state,const string song,const int timeLength,const string singer);
 	~Participant() = default;
 	Participant(Participant& p) = delete;
@@ -59,7 +65,7 @@ public:
 	~Voter() = default;
 	Voter(Voter& v) = delete;
 	
-	string state()const;
+	string state() const;
 	VoterType voterType() const;
 	int timesOfVotes() const;
 	
@@ -76,6 +82,7 @@ public:
 
 // -----------------------------------------------------------
 
+
 struct Vote
 {
 	Voter vr;
@@ -88,8 +95,6 @@ struct Vote
 
 	Vote(Vote& v) = delete;
 	Vote& operator=(const Vote&) = delete;
-
-	//todo: implement d'tor for arrays	
 
 	// ALL is public here.
 	// need to define ONLY data members and c'tr and d'tr.
@@ -104,9 +109,9 @@ class MainControl
 	// relevant private members can be defined here, if necessary.
 	Phase phase;
 
-	int maxSongLength;
-	int maxParticipants;
-	int maxRegularVotes;
+	const int maxSongLength;
+	const int maxParticipants;
+	const int maxRegularVotes;
 
 public:
 
@@ -114,12 +119,6 @@ public:
 	void setPhase(Phase phase);
 
 
-	/* the operation legalParticipant checks if all the following exists :
-   the names of the state and the song and the singer are not empty "",
-   as well as time length of the song is not big than the max allowed time length
-   for any song. this max allowed value is determined when defining the
-   system object. default is 180 (sec).
-*/
 	bool legalParticipant(Participant p);
 
 	//checks if the given state participates in the Eurovision or not
@@ -127,6 +126,18 @@ public:
 
 	MainControl& operator+=(const Participant& p);
 	MainControl& operator+=(const Vote& v);
+
+	/*
+  unregister p5 from the system.
+  it is allowed to do this only in the Registration phase.
+  Moreover, if p5 is not registered, then just ignore the operation without
+  further notice.
+
+  can also do operations like
+
+  (((eurovision -= p5) -= p3) += p4) -= p4;
+
+*/
 	MainControl& operator-=(const Vote& v);
 
 
@@ -139,7 +150,44 @@ public:
 };
 
 ostream& operator<<(ostream& os ,const Participant& p);
+
+
+//todo: remove all the comments below after done implementing the overload
+/*
+  displays the main features of the system.
+  All the display is between curly brackets ({ in the first line, then continue
+  to next line, then all the relevant data, then } in the last line.
+  first line after { displays the phase - can be one of Registration or Contest or Voting.
+  if phase is Registration, then all Registered participant are displayed
+  sorted regarding state name. each state in a different line, in the same format
+  as operator<< to a participant.
+  if phase is Contest, nothing more is displayed.
+  if phase is Voting, then all votes (both Regular and Judge) are displayed,
+  in respect to all states of registered participants.
+  each state in a different line. the states are sorted in respect to state name.
+
+  example :
+{
+Registration
+[Australia/Song_Australia/180/Singer_Australia]
+[Cyprus/Song_Cyprus/172/Singer_Cyprus]
+[Israel/Song_Israel/175/Singer_Israel]
+[UK/Song_UK/170/Singer_UK]
+}
+
+  another example relevant to Voting phase - explanation will follow later :
+{
+Voting
+Australia : Regular(0) Judge(8)
+Cyprus : Regular(6) Judge(12)
+Israel : Regular(1) Judge(0)
+UK : Regular(1) Judge(10)
+}
+
+*/
 ostream& operator<<(ostream& os, const MainControl& main_control);
+
+
 // -----------------------------------------------------------
 
 #endif
