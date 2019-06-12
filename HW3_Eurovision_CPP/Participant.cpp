3763daad71b627294ec1d8650a5c42e46b3eb0ed
Guy-El Fridman <guy-elf@campus.technion.ac.il>
2019-06-12 17:09:55 +0300

    bla

--- HW3_Eurovision_CPP/Participant.cpp
+++ HW3_Eurovision_CPP/Participant.cpp
@@ -1,57 +1,10 @@
+
+#include <string>
 #include "eurovision.h"
-#include <functional>
+using std::string;
+//
+//Participant::Participant(string state, string song, int timeLength, string singer)
+//{
+//	
+//}
 
-Participant::Participant(const string state, const string song, const int timeLength, const string singer)
-:state_name(state), song_name(song),song_duration(timeLength), singer_name(singer)
-{
-	is_registered = false;
-}
-
-bool Participant::isRegistered()const
-{
-	return this->is_registered;
-}
-
-string Participant::state() const
-{
-	return this->state_name;
-}
-
-string Participant::song() const
-{
-	return this->song_name;
-}
-
-string Participant::singer() const
-{
-	return this->singer_name;
-}
-
-int Participant::timeLength() const
-{
-	return this->song_duration;
-}
-
-
-void Participant::updateRegistered(bool status)
-{
-	this->is_registered = status;
-}
-
-void Participant::update(const string song, const int timeLength, const string singer)
-{
-	if(isRegistered()) return;
-
-	this->song_name = song;
-	this->song_duration = timeLength;
-	this->singer_name = singer;
-}
-
-//Note this function is not part of CLASS Participant but in charge of printing it
-ostream& operator<<(ostream& os,const Participant& p)
-{
-	string const part1 = "[" + p.state() + "/" + p.song()+ "/"; 
-	string const part2 = "/" + p.singer() + "]";
-
-	return os << part1 << p.timeLength() << part2;
-}
