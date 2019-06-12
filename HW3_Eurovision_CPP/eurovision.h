e607c23d53ce3946302f0614d7054238c751b369
Guy-El Fridman <guy-elf@campus.technion.ac.il>
2019-06-12 18:09:34 +0300

    Added functionality with Nir

--- HW3_Eurovision_CPP/eurovision.h
+++ HW3_Eurovision_CPP/eurovision.h
@@ -88,7 +88,6 @@ struct Vote
 	Voter vr;
 	string states [10];
 
-	Vote(Voter vr, string state);// remember to check for judge here too
 	Vote(Voter vr, string s1 = "", string s2 = "", string s3 = "", 
 		string s4 = "",string s5 = "", string s6 = "", string s7 = "", 
 		string s8 = "",string s9 = "", string s10 = ""); //overload for judge
