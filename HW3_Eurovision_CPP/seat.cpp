cb38b0c3ea4761dca441905df797471356a0b54e
Guy-El Fridman <guy-elf@campus.technion.ac.il>
2019-06-25 13:14:30 +0300

    Added GreenRoom and MainHall cpp

--- HW3_Eurovision_CPP/seat.cpp
+++ HW3_Eurovision_CPP/seat.cpp
@@ -1,3 +1,42 @@
 #include "eurovision.h"
 #include "seat.h"
 
+using std::to_string;
+
+
+//Seat
+int Seat::price() const
+{
+	return this->basePrice;
+}
+
+string Seat::location() const
+{
+	return "line: " + to_string(this->lineNum) + ", chair: " +
+				    to_string(this->chairNum);
+}
+
+
+//GreenRoom
+GreenRoomSeat::GreenRoomSeat(int line, int chair)
+:Seat(chair, line, -1){}
+
+int GreenRoomSeat::price() const
+{
+	throw NoPrice();
+}
+
+string GreenRoomSeat::location() const
+{
+	return "Green Room-> " + this->location();
+}
+
+
+//Main Hall
+MainHallSeat::MainHallSeat(int chair_num, int line_num, int base_price)
+						  :Seat(chair_num,line_num,base_price+100){}
+
+string MainHallSeat::location() const
+{
+	return "Main Hall-> " + this->location();
+}
--- HW3_Eurovision_CPP/seat.h
+++ HW3_Eurovision_CPP/seat.h
@@ -5,64 +5,79 @@ using std::exception;
 
 
 // ---------------------------------------------
-class NoPrice:exception
+class NoPrice:public exception
 {
+
+public:
+	NoPrice():exception("Not For Sale !"){}
+	virtual ~NoPrice()=default;
 };
 
 // ---------------------------------------------
 class Seat
 {
+
+protected:
 	int chairNum;
 	int lineNum;
 	int basePrice;
 
 public:
-	Seat(int chair_num, int line_num, int base_price);
-	virtual  ~Seat();
+	Seat(int chair_num, int line_num, int base_price): chairNum(chair_num),
+								  lineNum(line_num),basePrice(base_price){}
+	virtual ~Seat();
 
-	virtual string location() const;
-	virtual int price() const = 0;
-	
+	virtual int price() const;
+	virtual string location() const = 0;
 };
 
 // ---------------------------------------------
-class GreenRoomSeat:Seat
+class GreenRoomSeat:public Seat
 {
 public:
+	GreenRoomSeat(int line, int chair);
+	//override to throw exception
 	int price() const override;
+	string location() const override;
 };
 
+
 // ---------------------------------------------
-class MainHallSeat:Seat
+class MainHallSeat:public Seat
 {
-	int mainHallPrice;
 
 public:
-	MainHallSeat();
-	virtual ~MainHallSeat();
-	int price() const override;
+	//use the c'tor of Seat and add 100 to the base_price
+	MainHallSeat(int chair_num, int line_num, int base_price);
+	virtual ~MainHallSeat() = default;
+
+	string location() const override;
 };
 
+
 // ---------------------------------------------
-class SpecialSeat:MainHallSeat
+class SpecialSeat:public MainHallSeat
 {
 public:
-	int price() const override;
+
 };
 
 // ---------------------------------------------
-class GoldenCircleSeat:SpecialSeat
+class GoldenCircleSeat:public SpecialSeat
 {
 };
 
 // ---------------------------------------------
-class DisablePodiumSeat:SpecialSeat
+class DisablePodiumSeat:public SpecialSeat
 {
+
 };
 
 // ---------------------------------------------
-class RegularSeat:MainHallSeat
+class RegularSeat:public MainHallSeat
 {
+
+protected:
 	char area;
 public:
 	RegularSeat() = default;
@@ -72,17 +87,17 @@ public:
 };
 
 // ---------------------------------------------
-class FrontRegularSeat:RegularSeat
+class FrontRegularSeat:public RegularSeat
 {
 };
 
 // ---------------------------------------------
-class MiddleRegularSeat:RegularSeat
+class MiddleRegularSeat:public RegularSeat
 {
 };
 
 // ---------------------------------------------
-class RearRegularSeat:RegularSeat
+class RearRegularSeat:public RegularSeat
 {
 };
 
