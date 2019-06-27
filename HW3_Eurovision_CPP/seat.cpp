93806cbbc6f874ae8f56dc9f3cca3eba502aaeca
moradnir <49983799+moradnir@users.noreply.github.com>
2019-06-27 22:45:19 +0300

    Update seat.cpp

--- HW3_Eurovision_CPP/seat.cpp
+++ HW3_Eurovision_CPP/seat.cpp
@@ -2,38 +2,40 @@
 
 using std::to_string;
 
+//NoPrice-------------------------------------------------------------
 
-//NoPrice
 char const* NoPrice::what() const noexcept
 {
-	return "Not For Sale !";
+    return "Not For Sale !";
 }
 
-//Seat
+//Seat----------------------------------------------------------------
+
 int Seat::price() const
 {
-	return this->basePrice;
+    return this->basePrice;
 }
 
 string Seat::location() const
 {
-	return "line: " + to_string(this->lineNum) + ", chair: " +
-				    to_string(this->chairNum);
+    return "line: " + to_string(this->lineNum) + ", chair: " +
+           to_string(this->chairNum);
 }
 
-//GreenRoom
+//GreenRoom-----------------------------------------------------------
+
 int GreenRoomSeat::price() const
 {
-	throw NoPrice();
+    throw NoPrice();
 }
 
 string GreenRoomSeat::location() const
 {
-	return "Green Room-> " + Seat::location();
+    return "Green Room-> " + Seat::location();
 }
 
 
-//Main Hall
+//Main Hall-----------------------------------------------------------
 
 //c'tor defined in the .h file
 
@@ -41,58 +43,56 @@ string GreenRoomSeat::location() const
 
 //c'tor
 
-//Golden Circle
+//Golden Circle-------------------------------------------------------
+
 string GoldenCircleSeat::location() const
 {
-	return "Golden Circle-> " + SpecialSeat::location();
+    return "Golden Circle-> " + SpecialSeat::location();
 }
 
-//Disable Podium
+//DisablePodium-------------------------------------------------------
 //note base_price is fixed
 
-//c'tor for 3 params 
+//c'tor for 3 params
 //c'tor for 2 params
 
 
 string DisablePodiumSeat::location() const
 {
-	return "Disable Podium-> " + SpecialSeat::location();
+    return "Disable Podium-> " + SpecialSeat::location();
 }
 
 //price is fixed for 200
 int DisablePodiumSeat::price() const
 {
-	return 200;
+    return 200;
 }
 
-//RegularSeat-------------------------------------------------------------------
+//RegularSeat---------------------------------------------------------
 
 string RegularSeat::location() const
 {
-	string area_string = string(1,area);
-	return "area: " + area_string + ", " + MainHallSeat::location();
+    string area_string = string(1,area);
+    return "area: " + area_string + ", " + MainHallSeat::location();
 }
 
-
-//FrontRegularSeat--------------------------------------------------------------
+//FrontRegularSeat----------------------------------------------------
 
 string FrontRegularSeat::location() const
 {
-	return "Front-> " + RegularSeat::location();
+    return "Front-> " + RegularSeat::location();
 }
 
-
-//MiddleRegularSeat-------------------------------------------------------------
+//MiddleRegularSeat---------------------------------------------------
 
 string MiddleRegularSeat::location() const
 {
-	return "Middle-> " + RegularSeat::location();
+    return "Middle-> " + RegularSeat::location();
 }
 
-
-//RearRegularSeat---------------------------------------------------------------
+//RearRegularSeat-----------------------------------------------------
 
 string RearRegularSeat::location() const
 {
-	return "Rear-> " + RegularSeat::location();
+    return "Rear-> " + RegularSeat::location();
 }
