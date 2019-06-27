099727f560856ef02f515e364303b7e2be9d0636
moradnir <49983799+moradnir@users.noreply.github.com>
2019-06-27 22:42:28 +0300

    Update eurovision.h

--- HW3_Eurovision_CPP/eurovision.h
+++ HW3_Eurovision_CPP/eurovision.h
@@ -6,226 +6,211 @@
 #include <list>
 #include <iterator>
 
-// it's allowed to define here any using statements, according to needs.
-// do NOT define here : using namespace std;
+//--------------------------------------------------------------------
 
 using std::string;
 using std::ostream;
 using std::istream;
 using std::endl;
 using std::swap;
-//---------------------------------------------------
+
+//--------------------------------------------------------------------
+
 enum VoterType { All, Regular, Judge };
 enum Phase { Registration, Contest, Voting };
 
-
-//---------------------------------------------------
+//Participant---------------------------------------------------------
 
 class Participant
 {
-	// relevant private members can be defined here, if necessary.
-
-	const string state_name;
-	string song_name;
-	int song_duration;
-	string singer_name;
-	bool is_registered;
+    const string state_name;
+    string song_name;
+    int song_duration;
+    string singer_name;
+    bool is_registered;
 
 public:
 
-	string state()const;
-	string song() const;
-	int timeLength() const;
-	string singer() const;
-	bool isRegistered() const;
+    //returns the state name
+    string state()const;
+    //returns the song name
+    string song() const;
+    //returns the time length of the song
+    int timeLength() const;
+    //returns the singer name
+    string singer() const;
+    //returns whether the participated country is registered or not
+    bool isRegistered() const;
 
-	Participant(const string state, const string song, const int timeLength, const string singer);
-	~Participant() = default;
-	Participant(Participant& p) = delete;
+    Participant(const string state, const string song, const int timeLength,
+                const string singer);
+    ~Participant() = default;
+    Participant(Participant& p) = delete;
 
-	void updateRegistered(bool status);
-	void update(const string song, const int timeLength, const string singer);
+    //updating the registration status of the participant to the given status
+    void updateRegistered(bool status);
 
-
-	// need to define here possibly c'tr and d'tr and ONLY methods that
-	// are mentioned and demonstrated in the test example that has been published.
-	// NO OTHER METHODS SHOULD APPEAR HERE.
-
-	// NO friend is allowed here.
+     // updating the participant's values by the given values
+    void update(const string song, const int timeLength, const string singer);
 };
 
-//---------------------------------------------------
-
+//Voter---------------------------------------------------------------
 
 class Voter
 {
-	// relevant private members can be defined here, if necessary.
-	const string state_name;
-	VoterType voter_type;
-	int times_of_votes;
+    const string state_name;
+    VoterType voter_type;
+    int times_of_votes;
 
 public:
 
-	Voter(string state, VoterType voterType = Regular);
-	~Voter() = default;
-	Voter(Voter& v) = delete;
+    Voter(string state, VoterType voterType = Regular);
+    ~Voter() = default;
+    Voter(Voter& v) = delete;
 
-	string state() const;
-	VoterType voterType() const;
-	int timesOfVotes() const;
+    //returns the state name of the voter
+    string state() const;
+    //returns enum with the voter type
+    VoterType voterType() const;
 
-	Voter& operator++();
+    //returns the amount of times the voter voted
+    int timesOfVotes() const;
 
-	// need to define here possibly c'tr and d'tr and ONLY methods that
-	// are mentioned and demonstrated in the test example that has been published.
-	// NO OTHER METHODS SHOULD APPEAR HERE.
-
-	// NO friend is allowed here.
+    //update times of votes
+    Voter& operator++();
 
 };
 
+//ParticipantWVotes---------------------------------------------------
 
-// -----------------------------------------------------------
+//An array of registered participants including voting information
 struct ParticipantWVotes
 {
-	Participant* participant_ptr;
-	int reg_votes;
-	int judge_votes;
+    Participant* participant_ptr;
+    int reg_votes;
+    int judge_votes;
 
-	explicit ParticipantWVotes(Participant* p = nullptr, int regular_votes = 0, int judge_votes = 0);
-	//ParticipantWVotes(ParticipantWVotes& s) = delete;
-	~ParticipantWVotes() = default;
+    explicit ParticipantWVotes(Participant* p = nullptr, int regular_votes = 0,
+                               int judge_votes = 0);
+
+    ~ParticipantWVotes() = default;
 };
 
-//Helper function: swap for ParticipantWvotes
-//Helper Swap - still requires a bit of modification for it run.
-//void swap(struct ParticipantWithVotes* p1, struct ParticipantWithVotes* p2);
+//Vote----------------------------------------------------------------
 
-// -----------------------------------------------------------
 struct Vote
 {
-	Voter& vr;
-	string* states;
+    Voter& vr;
+    string* states;
 
-	Vote(Voter& vr, string s1 = "", string s2 = "", string s3 = "",
-		string s4 = "", string s5 = "", string s6 = "", string s7 = "",
-		string s8 = "", string s9 = "", string s10 = "");
+    Vote(Voter& vr, string s1 = "", string s2 = "", string s3 = "",
+         string s4 = "", string s5 = "", string s6 = "", string s7 = "",
+         string s8 = "", string s9 = "", string s10 = "");
 
-	Vote(Vote& v) = delete;
-	Vote& operator=(const Vote&) = delete;
-	~Vote();
-	// ALL is public here.
-	// need to define ONLY data members and c'tr and d'tr.
-	// NO NEED to define anything else.
+    Vote(Vote& v) = delete;
+    Vote& operator=(const Vote&) = delete;
+    ~Vote();
+
 };
 
-// -----------------------------------------------------------
-
+//MainControl---------------------------------------------------------
 
 class MainControl
 {
-	// relevant private members can be defined here, if necessary.
-	Phase phase;
+    // relevant private members can be defined here, if necessary.
+    Phase phase;
 
-	const int maxSongLength;
-	const int maxParticipants;
-	const int maxRegularVotes;
-	ParticipantWVotes* contest_arr;
+    const int maxSongLength;
+    const int maxParticipants;
+    const int maxRegularVotes;
+    ParticipantWVotes* contest_arr;
 
-	int _iterator = 0;
-	ParticipantWVotes* cur_iterator;
+    int _iterator = 0;
+    ParticipantWVotes* cur_iterator;
 
-	//checks if the contest array is full
-	bool isContestFull();
-	//checks if the given participant is already registered in the system
-	bool isParticipantRegistered(const Participant& participant);
-	int getParticipatingNum() const;
+    //checks if the contest array is full
+    bool isContestFull();
+
 public:
 
-	MainControl(int maxSongLength = 180, int maxParticipants = 26, int maxRegularVotes = 5);
-	void setPhase(Phase phase);
-	~MainControl();
+    MainControl(int maxSongLength = 180, int maxParticipants = 26,
+                int maxRegularVotes = 5);
+    //sets the phase of the contest
+    void setPhase(Phase phase);
+    ~MainControl();
 
-	bool legalParticipant(const Participant& p) const;
+    //checks if the given participant is legal
+    bool legalParticipant(const Participant& p) const;
 
-	//checks if the given state participates in the Eurovision or not
-	bool participate(string state_name) const;
+    //checks if the given state participates in the Eurovision or not
+    bool participate(string state_name) const;
 
-	MainControl& operator+=(Participant& participant);
-	MainControl& operator-=(Participant &participant);
+    //add a participant to the contest
+    MainControl& operator+=(Participant& participant);
+    //remove participant from the contest
+    MainControl& operator-=(Participant &participant);
 
-	MainControl& operator+=(const Vote& vote);
-	//B.3
-	string operator()(const int place, const VoterType vt) const;
+    //submits a vote of the given voter
+    MainControl& operator+=(const Vote& vote);
+    //B.3
+    string operator()(const int place, const VoterType vt) const;
 
+    //prints the eurovision details
+    friend ostream& operator<<(ostream& os, const MainControl& eurovision);
 
-	friend ostream& operator<<(ostream& os, const MainControl& eurovision);
+    typedef ParticipantWVotes* Iterator;
 
-	typedef ParticipantWVotes* Iterator;
-
-	Iterator begin();
-	Iterator end();
-	bool operator==(const Iterator& iterator) const;
-	Iterator& operator++();
-
+    //returns the beginning of the container
+    Iterator begin();
+    //returns the end of the container (1 place after the last element)
+    Iterator end();
+    //comparing iterators
+    bool operator==(const Iterator& iterator) const;
+    //advancing iterators
+    Iterator& operator++();
 };
 
+//section of print operators:
 ostream& operator<<(ostream& os, const Participant& p);
 ostream& operator<<(ostream& os, const Voter& v);
 ostream& operator<<(ostream& os, const MainControl& eurovision);
-
-
-//ostream& operator<<(ostream& os, const MainControl::Iterator& iterator);
 ostream& operator<<(ostream& os, const ParticipantWVotes& pwv);
-// -----------------------------------------------------------
-//Part B:
 
-
-
-
+//PartB---------------------------------------------------------------
 
 //B.1
 using std::list;
 template<typename T_Iterator>
-T_Iterator Get(T_Iterator begin, T_Iterator end, int place)
-{
-	//validity check
-	if (begin == end || place < 1)
-		return end;
+T_Iterator Get(T_Iterator begin, T_Iterator end, int place){
+    //validity check
+    if (begin == end || place < 1) return end;
 
-	int size = 0;
-	//gets the value the type that the iterator points to
-	list<typename std::iterator_traits<T_Iterator>::value_type> temp_list;
+    int size = 0;
+    //gets the value the type that the iterator points to
+    list<typename std::iterator_traits<T_Iterator>::value_type> temp_list;
 
-	for (T_Iterator i = begin; i != end; ++i)
-	{
-		temp_list.push_back(*i);
-		size++;
-	}
+    for (T_Iterator i = begin; i != end; ++i){
+        temp_list.push_back(*i);
+        size++;
+    }
 
-	//validity check- size is not available before the loop
-	if (size < place) //come back here to add the <= sign
-		return end;
-	//list is sorted in ascending order
-	temp_list.sort();
+    //validity check- size is not available before the loop
+    if (size < place) return end;
+    //list is sorted in ascending order
+    temp_list.sort();
 
-	for (int i = 0; i < place - 1; ++i)
-	{
-		temp_list.pop_back();
-	}
+    for (int i = 0; i < place - 1; ++i){
+        temp_list.pop_back();
+    }
 
-	typename std::iterator_traits<T_Iterator>::value_type result_value =
-		temp_list.back();
+    typename std::iterator_traits<T_Iterator>::value_type result_value =
+            temp_list.back();
 
-	for (T_Iterator i = begin; i != end; ++i)
-	{
-		if (*i == result_value)
-			return i;
-	}
-	return end;
+    for (T_Iterator i = begin; i != end; ++i){
+        if (*i == result_value) return i;
+    }
+    return end;
 }
-// -----------------------------------------------------------
-
-
 
 #endif
+
